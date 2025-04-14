076038a00d06fae0c191ceb4e9eb0419ed1964d0
lnettf <luisrnett@gmail.com>
2025-04-14 18:54:10 +0200

    fix: correct import path for Movies component and update route paths for movies and movie details

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,8 @@ import "./index.css";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router";
 import { Login } from "./modules/auth/login/Login";
 import { Register } from "./modules/auth/register/Register";
-import { Movies } from "./modules/movies/listMovies/movies";
 import { MoviesById } from "./modules/movies/byId/MoviesById";
+import { Movies } from "./modules/movies/listMovies/Movies";
 import { Guard } from "./modules/auth/guard/Guard";
 import "./utils/interceptor";
 import { ROUTES } from "./routes";
@@ -18,7 +18,7 @@ createRoot(document.getElementById("root")).render(
         <Route path={ROUTES.REGISTER} element={<Register />} />
 
         <Route
-          path="/movies"
+          path={ROUTES.MOVIES}
           element={
             <Guard>
               <Movies />
@@ -26,7 +26,7 @@ createRoot(document.getElementById("root")).render(
           }
         />
         <Route
-          path="/movies/:id"
+          path={ROUTES.MOVIE_DETAIL}
           element={
             <Guard>
               <MoviesById />
